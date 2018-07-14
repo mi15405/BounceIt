9e081ce08e9a510ff0993843a23117d81201119b
mi15405 <mi15405.git@gmail.com>
2018-07-14 21:15:32 +0100

    update

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -2,38 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ball : MonoBehaviour {
+public class Ball : MonoBehaviour
+{
 
-    [SerializeField]
-    private float gravity;
+	[SerializeField] 
+	private float gravity;
 
-    [SerializeField]
+	[SerializeField] 
 	private Transform startingPosition;
-    public Transform StartingPosition
-    {
-        get { return startingPosition; }
-        set { startingPosition = value; }
-    }
-
-    private Rigidbody rb;
-
-	void Start () 
-	{
-        rb = GetComponent<Rigidbody>();
-		MoveToStart();
-	}
 	
-	void FixedUpdate () 
+
+	public Transform StartingPosition
+	{
+		get { return startingPosition; }
+		set { startingPosition = value; }
+	}
+
+	private Rigidbody rb;
+
+	private void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		MoveToStart();
+		
+	}
+
+	private void FixedUpdate () 
 	{
 		if (gravity != 0f)
             rb.AddForce(Vector3.down * gravity * Time.deltaTime);
+		
 	}
 
 	public void MoveToStart()
 	{
 		transform.position = startingPosition.position;
 		rb.velocity = Vector3.zero;
-        FindObjectOfType<PlatformMovement>().ResetPosition();
+		
+		var platform = FindObjectOfType<PlatformMovement>();
+		if (platform != null)
+			platform.ResetPosition();
 	}
 
 }
---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+	
+    // ROTATE
+	[SerializeField] 
+	private float rotateSpeed;
+
+	// SHOOT
+	[SerializeField] 
+	private Transform aimFrom;
+
+	[SerializeField] 
+	private float forceChargeTime;
+	private float forceCharge;
+	
+	[SerializeField]
+	private float minFireForce;
+	
+	[SerializeField]
+	private float maxFireForce;
+	
+	private float fireForce;
+
+	private bool shootPressed;
+	private bool shootReleased;
+	
+	private Vector3 fireDirection = Vector3.forward;
+
+	private Rigidbody rb;
+
+	private void Awake()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	private void Start()
+	{
+		fireForce = minFireForce;
+		forceCharge = (maxFireForce - minFireForce) / forceChargeTime;
+	}
+
+	private void Update()
+	{
+		GetInput();
+		UpdateAim();
+		Fire();	
+	}
+
+	private void GetInput()
+	{
+		fireDirection.z = Input.GetAxis("Horizontal");
+		fireDirection.y = Input.GetAxis("Vertical");
+		shootPressed = Input.GetButton("Jump");
+		shootReleased = Input.GetButtonUp("Jump");	
+	}
+	
+	private void UpdateAim()
+	{
+		// Set position to ball's position
+		aimFrom.position = transform.position;
+		
+		if (fireDirection == Vector3.zero)
+			return;
+
+		var targetRotation = Vector3.RotateTowards(aimFrom.forward, fireDirection, rotateSpeed * Time.deltaTime, 0f);
+		aimFrom.rotation = Quaternion.LookRotation(targetRotation);
+	}
+
+	private void Fire()
+	{
+        if (shootReleased)
+        {
+	        // Limit force value
+	        fireForce = Mathf.Clamp(fireForce, minFireForce, maxFireForce);
+	        
+			rb.AddForce(aimFrom.forward * fireForce, ForceMode.Impulse);
+	        fireForce = minFireForce;
+        }	
+		else if (shootPressed)
+		{
+			fireForce += forceCharge * Time.deltaTime;
+		}
+		
+	}
+}
